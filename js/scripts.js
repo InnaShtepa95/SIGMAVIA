956f95ea23e95ae19d0a351051cd96b54ad2a66c
InnaShtepa95 <inna.shtepa@gmail.com>
2019-06-08 14:36:13 +0300

    create flight section

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
+(function () {
+    'use strict';
 
-if(window.devicePixelRatio > 1){
-    var multiplier = window.devicePixelRatio;
-    var images = document.querySelectorAll('img.optimizer');
-    images.forEach(function(el){
-        var sizeX = el.width;
-        var sizeY = el.height;
-        var imageType = el.src.substr(-4);
-        var imageName = el.src.substr(0, el.src.length - 4);
-        imageName += "_x" + multiplier + imageType;
-        // imageName += "@" + multiplier + 'x' + imageType;
-        var newImg = new Image();
-        newImg.src = imageName;
-        newImg.onload = function() {
-            el.src = imageName;
-            el.width = sizeX;
-            el.height = sizeY;
-        }
-    });
-};
+    var init = function () {
 
-$(document).ready(function(){
-    $("table").clone().appendTo('.somnewtable');
-});
+
+
+        var slider3 = new rSlider({
+            target: '#slider3',
+            values: {min: 0, max: 2000},
+            step: 1,
+            range: true,
+            set: [100, 1000],
+            scale: true,
+            // labels: true,
+
+            //set:    null, // an array of preselected values
+            width:    null,
+            // labels:   true,
+            tooltip:  true,
+            onChange: function (vals) {
+                console.log(vals);
+            }
+        });
+
+
+    };
+    window.onload = init;
+})();
