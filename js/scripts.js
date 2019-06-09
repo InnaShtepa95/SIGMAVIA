87bd3d9b3a74f2552d7773a196abc70835b3bca6
InnaShtepa95 <inna.shtepa@gmail.com>
2019-06-09 18:46:02 +0300

    add script for images

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,28 @@
     };
     window.onload = init;
 })();
+
+
+if(window.devicePixelRatio > 1){
+    var multiplier = window.devicePixelRatio;
+    var images = document.querySelectorAll('img.optimizer');
+    images.forEach(function(el){
+        var sizeX = el.width;
+        var sizeY = el.height;
+        var imageType = el.src.substr(-4);
+        var imageName = el.src.substr(0, el.src.length - 4);
+        //imageName += "_x" + multiplier + imageType;
+        imageName += "@" + multiplier + 'x' + imageType;
+        var newImg = new Image();
+        newImg.src = imageName;
+        newImg.onload = function() {
+            el.src = imageName;
+            el.width = sizeX;
+            el.height = sizeY;
+        }
+    });
+};
+
+$(document).ready(function(){
+    $("table").clone().appendTo('.somnewtable');
+});
